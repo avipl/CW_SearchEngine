4718aaedde167bf32706c7718ef40107d2d46aee
Avinash Lakhmawad <avipl1994@gmail.com>
2023-03-15 22:17:01 -0700

    add index search tbc..

--- imdb_app/bert.py
+++ imdb_app/bert.py
@@ -1,8 +1,62 @@
-def bert_query(query):
-    li=[]
-    li.append(["RRR","0.55", "2022", "Rajamouli","https://www.imdb.com/title/tt8178634/"])
-    li.append(["Pathaan","0.33","2023","Siddharth Anand","https://www.imdb.com/title/tt12844910/"])
-    li.append(["RRR","0.55", "2022", "Rajamouli","https://www.imdb.com/title/tt8178634/"])
-    li.append(["Pathaan","0.33","2023","Siddharth Anand","https://www.imdb.com/title/tt12844910/"])
-    return li
+from transformers import AutoTokenizer, AutoModel
+import torch
+from sklearn.metrics.pairwise import cosine_similarity
+import faiss                   # make faiss available
+import pandas as pd
+import os, shutil
+import sys
+import json
+import math
+
+def convert_to_embedding(query):
+    tokenizer = AutoTokenizer.from_pretrained('sentence-transformers/all-distilroberta-v1') # you can change the model here
+    model = AutoModel.from_pretrained('sentence-transformers/all-distilroberta-v1')
+    
+    tokens = {'input_ids': [], 'attention_mask': []}
+    new_tokens = tokenizer.encode_plus(query, max_length=512,
+                                       truncation=True, padding='max_length',
+                                       return_tensors='pt')
+    tokens['input_ids'].append(new_tokens['input_ids'][0])
+    tokens['attention_mask'].append(new_tokens['attention_mask'][0])
+    tokens['input_ids'] = torch.stack(tokens['input_ids'])
+    tokens['attention_mask'] = torch.stack(tokens['attention_mask'])
+    with torch.no_grad():
+        outputs = model(**tokens)
+    embeddings = outputs.last_hidden_state
+    attention_mask = tokens['attention_mask']
+    mask = attention_mask.unsqueeze(-1).expand(embeddings.size()).float()
+    masked_embeddings = embeddings * mask
+    summed = torch.sum(masked_embeddings, 1)
+    summed_mask = torch.clamp(mask.sum(1), min=1e-9)
+    mean_pooled = summed / summed_mask
+    
+    return mean_pooled[0] # assuming query is a single sentence
+
+def bert_query(query, k = 5):
+    query_embedding = convert_to_embedding(query)
+    index_loaded = faiss.read_index("bert_test.index")
+    scores, indices = index_loaded.search(query_embedding[None, :], k)
+    topkdocs = []
+    cnt = 0;
+    for i in indices[0]:
+        if i >= 0:
+            topkdocs.append({
+                "score": str(scores[0][cnt]),
+                "name": df.iloc[i]["movie_name"],
+                "year": str(df.iloc[i]["year"]),
+                "rating": str(df.iloc[i]["rating"]),
+                "votes": str(df.iloc[i]["votes"]),
+                "runtime": str(df.iloc[i]["runtime"]),
+                "certificate": df.iloc[i]["certificate"],
+                "cast": df.iloc[i]["cast"],
+                "director": df.iloc[i]["director"],
+                "genre": df.iloc[i]["genre"],
+                "plot": df.iloc[i]["plot"],
+                "movie_id": df.iloc[i]["movie_id"]
+            })
+        cnt = cnt + 1
+    return json.dumps(topkdocs)
+    
+
+    
 
--- imdb_app/lucene.py
+++ imdb_app/lucene.py
@@ -1,7 +1,62 @@
-def lucene_query(query):
-    li=[]
-    li.append(["RRR","0.55", "2022", "Rajamouli","https://www.imdb.com/title/tt8178634/"])
-    li.append(["Pathaan","0.33","2023","Siddharth Anand","https://www.imdb.com/title/tt12844910/"])
-    li.append(["RRR","0.55", "2022", "Rajamouli","https://www.imdb.com/title/tt8178634/"])
-    li.append(["Pathaan","0.33","2023","Siddharth Anand","https://www.imdb.com/title/tt12844910/"])
-    return li
+
+class CustomQueryParser(PythonQueryParser):
+    def __init__(self, f, a):
+        super(CustomQueryParser, self).__init__(f, a)
+    
+    def getFieldQuery_quoted(_self, field, queryText, quoted):
+        query =  super(CustomQueryParser, _self).getFieldQuery_quoted_super(field, queryText, quoted)
+        if "votes" == field or "runtime" == field or "year" == field:
+            if queryText is not None: queryText = int(queryText)
+            return IntPoint.newExactQuery(field, queryText)
+        
+        if "rating" == field:
+            if queryText is not None: queryText = float(queryText)
+            return FloatPoint.newExactQuery(field, queryText)
+        
+        return query
+        
+    def getRangeQuery(_self, field, part1, part2, lowerInclusive, upperInclusive):
+        query = super(CustomQueryParser, _self).getRangeQuery(field, part1, part2, lowerInclusive, upperInclusive)
+        if "votes" == field or "runtime" == field or "year" == field:
+            if part1 is not None: part1 = int(part1)
+            else: part1 = 0 #votes and runtime are non-negative integers
+            if part2 is not None: part2 = int(part2)
+            else: part2 = 2147483647 # INT32 max
+            return IntPoint.newRangeQuery(field, part1, part2)
+        
+        if "rating" == field:
+            if part1 is not None: part1 = float(part1)
+            else: part1 = 0.0 #rating is non-negative float
+            if part2 is not None: part2 = float(part2)
+            else: part2 = 10.0 # max rating 10.0
+            return FloatPoint.newRangeQuery(field, part1, part2)
+        
+        return query
+
+
+def lucene_query(query, k=5):
+    searcher = IndexSearcher(DirectoryReader.open('./lucene_index'))
+    
+    parser = CustomQueryParser('all_fields', StandardAnalyzer())
+    parsed_query = parser.parse(query)
+    
+    topDocs = searcher.search(parsed_query, k).scoreDocs
+    topkdocs = []
+    for hit in topDocs:
+        doc = searcher.doc(hit.doc)
+        topkdocs.append({
+            "score": hit.score,
+            "name": doc.get("name"),
+            "year": doc.get("year"),
+            "rating": doc.get("rating"),
+            "votes": doc.get("votes"),
+            "runtime": doc.get("runtime"),
+            "certificate": doc.get("certificate"),
+            "cast": doc.get("cast"),
+            "director": doc.get("director"),
+            "genre": doc.get("genre"),
+            "plot": doc.get("plot"),
+            "movie_id": doc.get("movie_id")
+        })
+        
+    return json.dumps(topkdocs)
